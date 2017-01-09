d5f5cd501983ffca8f69bc9f6faae775017c8a53
Thiago Cunha <thiagocunha.java@gmail.com>
2017-01-09 04:17:55 -0200

    Diretório de módulos

---
+++ _modulos/suporte-tecnico.php
@@ -0,0 +1,13 @@
+<div class="btn-group pull-right m-t-15">
+    <button type="button" class="btn btn-custom dropdown-toggle waves-effect waves-light"
+    data-toggle="dropdown" aria-expanded="false">Opções Avançadas <span class="m-l-5"><i
+        class="fa fa-cog"></i></span></button>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+        </div>
+
+</div>
